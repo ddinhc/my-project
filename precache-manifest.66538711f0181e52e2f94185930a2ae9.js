4b77fabeba4909a7526a0617ef93495446503ce2
Duy Dinh <ddinh8@ohlone.edu>
2019-05-21 16:17:43 -0700

    Updates

---
+++ precache-manifest.66538711f0181e52e2f94185930a2ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed1ce64aed21b97d558e3a3c0cb08cf",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "ead910d640aa038709ab",
+    "url": "/my-project/static/css/main.6c6a3da8.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/my-project/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ead910d640aa038709ab",
+    "url": "/my-project/static/js/main.f40185ff.chunk.js"
+  },
+  {
+    "revision": "02869910549d422a2b1d",
+    "url": "/my-project/static/js/runtime~main.8ce5ffd9.js"
+  },
+  {
+    "revision": "a193b7b49373a9bbaeab963cdc6b9433",
+    "url": "/my-project/static/media/jumbotron.a193b7b4.jpg"
+  }
+]);
